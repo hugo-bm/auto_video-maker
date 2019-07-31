b0d2bfc8d405e242bd647e2f837b839eadbf24e8
Your Name <you@example.com>
2019-07-31 15:39:02 -0300

    'keywords_by_Watson'

---
+++ modules/content.js
@@ -0,0 +1,18 @@
+const content = {
+    searchTerms: String,
+    prefix: String,
+    lang: String,
+    maximumSentences: 9,
+    sourceContentOriginal: String,
+    sourceContentSanitized: String,
+    sentences: [
+        //modelo de formato de dados
+        //{
+       // text: String,
+       // keywords: [String],
+       // images: [String]
+   // }
+]
+}
+
+module.exports = content
